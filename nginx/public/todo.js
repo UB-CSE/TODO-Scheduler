cf0103d452026e64d5fc422823e84fd3eac99e0d
agwoska <agwoska@buffalo.edu>
2020-04-23 23:33:14 -0400

    first commit: frontend change; adds functionality

--- nginx/public/todo.js
+++ nginx/public/todo.js
@@ -12,7 +12,7 @@ function displayTasks(tasksJSON) {
     let formattedTasks = "";
     for (const task of tasks) {
         formattedTasks += "<hr/>";
-        formattedTasks += "<b>" + task['title'] + "</b> - " + task['description'] + "<br/>";
+        formattedTasks += "<b>" + task['title'] + "</b> - " + task['description'] + task['date'] + "<br/>";
         formattedTasks += "<button onclick='completeTask(\"" + task['id'] + "\")'>Task Complete</button>";
     }
     document.getElementById("tasks").innerHTML = formattedTasks;
@@ -22,9 +22,13 @@ function displayTasks(tasksJSON) {
 function addTask() {
     let title = document.getElementById("title").value;
     let desc = document.getElementById("desc").value;
-    socket.emit("add_task", JSON.stringify({"title": title, "description": desc}));
+    let date = document.getElementById("datepicker").value;
+    let priority = document.getElementById("priority").value;
+    socket.emit("add_task", JSON.stringify({"title": title, "description": desc, "date": date, "priority": priority}));
     document.getElementById("title").value = "";
     document.getElementById("desc").value = "";
+    document.getElementById("datepicker").value = "";
+    document.getElementById("priority").value = "0"
 }
 
 function completeTask(taskId) {
