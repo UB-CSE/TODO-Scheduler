6f2c4e2772e8528058f456bae86ed366bae89aa9
Brett <brettsit@buffalo.edu>
2020-04-25 20:41:18 -0400

    Project Contribution - Add task date & time

--- nginx/public/todo.js
+++ nginx/public/todo.js
@@ -12,7 +12,10 @@ function displayTasks(tasksJSON) {
     let formattedTasks = "";
     for (const task of tasks) {
         formattedTasks += "<hr/>";
-        formattedTasks += "<b>" + task['title'] + "</b> - " + task['description'] + "<br/>";
+
+        // The line below was changed to display the new information (date and time a task was added)
+        formattedTasks += "<b>" + task['title'] + "</b> - " + "<b>" + task['description'] + "</b> - " + task['time'] + "<br/>";
+
         formattedTasks += "<button onclick='completeTask(\"" + task['id'] + "\")'>Task Complete</button>";
     }
     document.getElementById("tasks").innerHTML = formattedTasks;
