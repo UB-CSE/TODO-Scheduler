b4723f8ddba7846a71fbf62e4098a7681d3f70ad
hope-kara <hopekara@buffalo.edu>
2020-04-26 22:16:32 -0400

    Added priority functionality. Can choose high, med, or low priority. If nothing is selected then it will say "none."

--- nginx/public/todo.js
+++ nginx/public/todo.js
@@ -12,7 +12,7 @@ function displayTasks(tasksJSON) {
     let formattedTasks = "";
     for (const task of tasks) {
         formattedTasks += "<hr/>";
-        formattedTasks += "<b>" + task['title'] + "</b> - " + task['description'] + "<br/>";
+        formattedTasks += "<b>" + task['title'] + "</b> - " + task['description'] + "<br/>" + "Priority: " + task['priority'] + "<br/>";
         formattedTasks += "<button onclick='completeTask(\"" + task['id'] + "\")'>Task Complete</button>";
     }
     document.getElementById("tasks").innerHTML = formattedTasks;
@@ -22,9 +22,19 @@ function displayTasks(tasksJSON) {
 function addTask() {
     let title = document.getElementById("title").value;
     let desc = document.getElementById("desc").value;
-    socket.emit("add_task", JSON.stringify({"title": title, "description": desc}));
+    let choices = document.getElementsByName("priority"); //gets all 3 choices in a collection
+    let priority = "none"; //"none" if no priority selected
+    let priorityId;
+    for (option of choices) { //iterate through high, med, low
+        if (option.checked) { //find which one was checked
+            priority = option.value; //save reference to value
+            priorityId = option.id //save reference to id to unselect after submitting
+        }
+    }
+    socket.emit("add_task", JSON.stringify({"title": title, "description": desc, "priority": priority})); //added priority
     document.getElementById("title").value = "";
     document.getElementById("desc").value = "";
+    document.getElementById(priorityId).checked = false; //uncheck priority selection
 }
 
 function completeTask(taskId) {
