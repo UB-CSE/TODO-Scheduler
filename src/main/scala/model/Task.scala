d2d3d6e2f38e3ee9bb5cd1d97eff63820002bd9f
etvanlieshout <ericvanl@buffalo.edu>
2020-05-02 14:31:06 -0400

    Being a person is hard; being a person in 116 is harder.
    Your friends and loved ones have been happy to support you throughout the semester, but now it's time for you to start pulling your weight around the apartment again. No more 116, no more excuses.
    
    Adds two default chore lists: daily & weekly. Users can opt-in for each by clicking a checkbox. Unclicking checkbox removes associated chores,
    Default chores lists are in Task object, and can easily be modified or updated to work with expanded functionality

--- src/main/scala/model/Task.scala
+++ src/main/scala/model/Task.scala
@@ -24,7 +24,34 @@ object Task {
     new Task(cleanString(title), cleanString(description, 1000), thisId.toString)
   }
 
+  /** DEFAULT TASKS: CHORES
+   * adding this here so that as people change the task params (timestamp, time to complete, etc
+   * these can be quickly updated at the same time, without searching through other parts of the code
+   * I realize I can do Task(title, description) instead of using apply, but it helps me understand
+   * apply better since I'm still a little unclear about scala's apply functionality
+   * ALSO: anyone who changes the params for Task, use default values so this doesn't all break
+   *  or need to be updated everytime
+   */
+  val dailyChores: List[Task] = List(
+    Task.apply("Dishes", "Go wash the dishes in the sink"),
+    Task.apply("Make Bed", "This is just basic"),
+    Task.apply("Tidy up", "Tidy up your work area, pick your clothes off the floor"),
+    Task.apply("Take out Trash", "Pick up all the trash and take it to the dumpster"),
+    Task.apply("Shower", "Everyday. And change your clothes"),
+    Task.apply("Drink Water", "Coffee is not water. Soda is not water."))
 
+  val weeklyChores: List[Task] = List(
+    Task.apply("Laundry", "Wash AND fold"),
+    Task.apply("Clean Bathroom", "Before you need a professional hazmat team"))
+
+  /** version with added time_to_complete (in hours) param, per swisstackle's contribution */
+  /**   val chores: List[Task] = List(
+    Task.apply("Dishes", "Go wash the dishes in the sink", 0.25),
+    Task.apply("Make Bed", "This is just basic", 0.10),
+    Task.apply("Tidy up", "Tidy up your work area, pick your clothes off the floor", 0.50),
+    Task.apply("Take out Trash", "Pick up all the trash and take it to the dumpster", 0.18),
+    Task.apply("Shower", "Everyday. And change your clothes", 0.33),
+    Task.apply("Drink Water", "Coffee is not water. Soda is not water.", 0.016))*/
 }
 
 class Task(val title: String, val description: String, val id: String) {
