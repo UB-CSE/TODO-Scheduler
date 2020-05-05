f765005f6d040478dc9b256b35e6468740abcafa
alvintsang <alvintsa@buffallo.edu>
2020-05-05 18:20:19 -0400

    Added a deadline functionality and a date created functionality.

--- src/main/scala/model/database/Database.scala
+++ src/main/scala/model/database/Database.scala
@@ -17,16 +17,18 @@ class Database extends DatabaseAPI{
 
   def setupTable(): Unit = {
     val statement = connection.createStatement()
-    statement.execute("CREATE TABLE IF NOT EXISTS tasks (title TEXT, description TEXT, id TEXT)")
+    statement.execute("CREATE TABLE IF NOT EXISTS tasks (title TEXT, description TEXT, date TEXT, created TEXT, id TEXT)")
   }
 
 
   override def addTask(task: Task): Unit = {
-    val statement = connection.prepareStatement("INSERT INTO tasks VALUE (?, ?, ?)")
+    val statement = connection.prepareStatement("INSERT INTO tasks VALUE (?, ?, ?, ?, ?)")
 
     statement.setString(1, task.title)
     statement.setString(2, task.description)
-    statement.setString(3, task.id)
+    statement.setString(3, task.date)
+    statement.setString(4, task.created)
+    statement.setString(5, task.id)
 
     statement.execute()
   }
@@ -50,8 +52,10 @@ class Database extends DatabaseAPI{
     while (result.next()) {
       val title = result.getString("title")
       val description = result.getString("description")
+      val date = result.getString("date")
+      val created = result.getString("created")
       val id = result.getString("id")
-      tasks = new Task(title, description, id) :: tasks
+      tasks = new Task(title, description, date, created, id) :: tasks
     }
 
     tasks.reverse
