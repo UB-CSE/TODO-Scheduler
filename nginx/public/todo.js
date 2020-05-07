50f6e8605d50528f63e7ab35ba6b2d1d60fa37d1
aacampbe <aacampbe@buffalo.edu>
2020-05-07 09:04:12 -0400

    Adding the current date and confirming if a task is completed

--- nginx/public/todo.js
+++ nginx/public/todo.js
@@ -12,7 +12,7 @@ function displayTasks(tasksJSON) {
     let formattedTasks = "";
     for (const task of tasks) {
         formattedTasks += "<hr/>";
-        formattedTasks += "<b>" + task['title'] + "</b> - " + task['description'] + "<br/>";
+        formattedTasks += "<b>" + task['title'] + "</b> - " + task['description'] + "</b> - " +  task['date'] + "<br/>";
         formattedTasks += "<button onclick='completeTask(\"" + task['id'] + "\")'>Task Complete</button>";
     }
     document.getElementById("tasks").innerHTML = formattedTasks;
@@ -26,7 +26,8 @@ function addTask() {
     document.getElementById("title").value = "";
     document.getElementById("desc").value = "";
 }
-
 function completeTask(taskId) {
-    socket.emit("complete_task", taskId);
+    if(confirm("Are you sure you completed the task?")){
+        socket.emit("complete_task", taskId);
+    }
 }
