a418975b7fe3cc5510e4a635590bbd1e47a533c0
ikondra <56898088+ikondra@users.noreply.github.com>
2020-05-09 11:50:43 -0400

    Added time and date that task was started/completed.

--- nginx/public/todo.js
+++ nginx/public/todo.js
@@ -2,6 +2,7 @@ const socket = io.connect("http://localhost:8080", {transports: ['websocket']});
 
 socket.on('all_tasks', displayTasks);
 socket.on('message', displayMessage);
+socket.on('complete', displayCompleted);
 
 function displayMessage(newMessage) {
     document.getElementById("message").innerHTML = newMessage;
@@ -14,10 +15,21 @@ function displayTasks(tasksJSON) {
         formattedTasks += "<hr/>";
         formattedTasks += "<b>" + task['title'] + "</b> - " + task['description'] + "<br/>";
         formattedTasks += "<button onclick='completeTask(\"" + task['id'] + "\")'>Task Complete</button>";
+        formattedTasks += "<br><b>" + "Started: " + task['dateTime'] + "</b>";
     }
     document.getElementById("tasks").innerHTML = formattedTasks;
 }
 
+function displayCompleted(tasksJSON) {
+    let tasks = JSON.parse(tasksJSON);
+    let formattedTasks = "";
+    for (const task of tasks) {
+        formattedTasks += "<hr/>";
+        formattedTasks += "<b> <i>COMPLETED</i>: " + task['title'] + "</b> - " + task['description'] + "<br/>";
+        formattedTasks += "<br><b>" + "Completed: " + task['dateTime'] + "</b>";
+    }
+    document.getElementById("completed").innerHTML = formattedTasks;
+}
 
 function addTask() {
     let title = document.getElementById("title").value;
