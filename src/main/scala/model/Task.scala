05336d2538637c810ebb3b4641efaa1771550dd9
Thomas Westpfal <westpfal385@gmail.com>
2020-05-08 21:44:12 -0400

    Added timestamps to each message. Added a name input field to the page. Added author's name to each message, with the ability to be an anonymous without entering a name.

--- src/main/scala/model/Task.scala
+++ src/main/scala/model/Task.scala
@@ -24,15 +24,42 @@ object Task {
     new Task(cleanString(title), cleanString(description, 1000), thisId.toString)
   }
 
+  def apply(title: String, description: String, author: String): Task = {
+    val thisId = nextId
+    nextId += 1
+    new Task(cleanString(title), cleanString(description, 1000), thisId.toString, author)
+  }
+
 
 }
 
 class Task(val title: String, val description: String, val id: String) {
 
+  val timeCreated: TimeInstant = TimeInstant.getNow
+  var author: String = ""
+
+  def this(title: String, description: String, id: String, author: String) = {
+    this(title, description, id)
+    this.addAuthor(author)
+  }
+
+  def addAuthor(name: String): Unit = {
+    author = name.strip()
+  }
+
+  // Return the author's name if one exists otherwise make the author
+  // "Anonymous".
+  def formatAuthor: String = {
+    if(author == null || author.strip() == "") { "Anonymous" }
+    else { author }
+  }
+
   def asJsValue(): JsValue ={
     val taskMap: Map[String, JsValue] = Map(
       "title" -> Json.toJson(title),
       "description" -> Json.toJson(description),
+      "timeCreated" -> Json.toJson(timeCreated.getDateTime(" - Created on ", " at ", "")),
+      "author" -> Json.toJson(formatAuthor),
       "id" -> Json.toJson(id)
     )
     Json.toJson(taskMap)
--- src/main/scala/model/TodoServer.scala
+++ src/main/scala/model/TodoServer.scala
@@ -69,8 +69,9 @@ class AddTaskListener(server: TodoServer) extends DataListener[String] {
     val task: JsValue = Json.parse(taskJSON)
     val title: String = (task \ "title").as[String]
     val description: String = (task \ "description").as[String]
+    val author: String = (task \ "author").as[String]
 
-    server.database.addTask(Task(title, description))
+    server.database.addTask(Task(title, description, author))
     server.server.getBroadcastOperations.sendEvent("all_tasks", server.tasksJSON())
   }
 
