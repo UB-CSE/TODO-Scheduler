6cf2dabef14c617a697eaf7a55dc56acae18427f
Ibrahim Gilani <isgilani@buffalo.edu>
2020-05-08 22:12:18 -0400

    added a priority text input

--- src/main/scala/model/Task.scala
+++ src/main/scala/model/Task.scala
@@ -18,22 +18,23 @@ object Task {
     output
   }
 
-  def apply(title: String, description: String): Task = {
+  def apply(title: String, description: String, priority: String): Task = {
     val thisId = nextId
     nextId += 1
-    new Task(cleanString(title), cleanString(description, 1000), thisId.toString)
+    new Task(cleanString(title), cleanString(description, 1000), thisId.toString, priority)
   }
 
 
 }
 
-class Task(val title: String, val description: String, val id: String) {
+class Task(val title: String, val description: String, val id: String, val priority: String) {
 
-  def asJsValue(): JsValue ={
+  def asJsValue(): JsValue = {
     val taskMap: Map[String, JsValue] = Map(
       "title" -> Json.toJson(title),
       "description" -> Json.toJson(description),
-      "id" -> Json.toJson(id)
+      "id" -> Json.toJson(id),
+      "priority" -> Json.toJson(priority)
     )
     Json.toJson(taskMap)
   }
