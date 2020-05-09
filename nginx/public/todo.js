b14223789b935b4ccbc5ab4b283b5e776f7b2b2b
jlee265 <jlee265@buffalo.edu>
2020-05-08 23:32:53 -0400

    Added Saving Task

--- nginx/public/todo.js
+++ nginx/public/todo.js
@@ -2,6 +2,7 @@ const socket = io.connect("http://localhost:8080", {transports: ['websocket']});
 
 socket.on('all_tasks', displayTasks);
 socket.on('message', displayMessage);
+socket.on('task_does_not_exist', displayPopup)
 
 function displayMessage(newMessage) {
     document.getElementById("message").innerHTML = newMessage;
@@ -29,4 +30,24 @@ function addTask() {
 
 function completeTask(taskId) {
     socket.emit("complete_task", taskId);
+}
+
+function saveTask() {
+    let title = document.getElementById("title").value;
+    let desc = document.getElementById("desc").value;
+    socket.emit("save_task", JSON.stringify({"title": title, "description": desc}));
+    document.getElementById("title").value = "";
+    document.getElementById("desc").value = "";
+}
+
+function addFromSavedTasks() {
+    let title = document.getElementById("title").value;
+    let desc = document.getElementById("desc").value;
+    socket.emit("add_from_saved_tasks", JSON.stringify({"title": title, "description": desc}));
+    document.getElementById("title").value = "";
+    document.getElementById("desc").value = "";
+}
+
+function displayPopup(TaskTitle) {
+    alert("Task title named '"+TaskTitle+"' does noct exist! Save new Task!");
 }
