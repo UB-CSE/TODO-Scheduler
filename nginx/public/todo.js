347fcb29fa577ef3c17c2658aa3b5d85d721d2c8
winso <winsonch@buffalo.edu>
2020-05-09 21:25:50 -0400

    Added deadline

--- nginx/public/todo.js
+++ nginx/public/todo.js
@@ -12,7 +12,7 @@ function displayTasks(tasksJSON) {
     let formattedTasks = "";
     for (const task of tasks) {
         formattedTasks += "<hr/>";
-        formattedTasks += "<b>" + task['title'] + "</b> - " + task['description'] + "<br/>";
+        formattedTasks += "<b>" + task['title'] + "</b> - " + task['description'] + "<br/> - " + task["deadline"] + "<br/>";
         formattedTasks += "<button onclick='completeTask(\"" + task['id'] + "\")'>Task Complete</button>";
     }
     document.getElementById("tasks").innerHTML = formattedTasks;
@@ -22,9 +22,11 @@ function displayTasks(tasksJSON) {
 function addTask() {
     let title = document.getElementById("title").value;
     let desc = document.getElementById("desc").value;
-    socket.emit("add_task", JSON.stringify({"title": title, "description": desc}));
+    let dead = document.getElementById("dead").value;
+    socket.emit("add_task", JSON.stringify({"title": title, "description": desc, "deadline": dead}));
     document.getElementById("title").value = "";
     document.getElementById("desc").value = "";
+    document.getElementById("dead").value = "";
 }
 
 function completeTask(taskId) {
--- src/main/scala/model/Task.scala
+++ src/main/scala/model/Task.scala
@@ -18,21 +18,22 @@ object Task {
     output
   }
 
-  def apply(title: String, description: String): Task = {
+  def apply(title: String, description: String, deadline: String): Task = {
     val thisId = nextId
     nextId += 1
-    new Task(cleanString(title), cleanString(description, 1000), thisId.toString)
+    new Task(cleanString(title), cleanString(description, 1000), cleanString(deadline), thisId.toString)
   }
 
 
 }
 
-class Task(val title: String, val description: String, val id: String) {
+class Task(val title: String, val description: String, val deadline: String, val id: String) {
 
   def asJsValue(): JsValue ={
     val taskMap: Map[String, JsValue] = Map(
       "title" -> Json.toJson(title),
       "description" -> Json.toJson(description),
+      "deadline" -> Json.toJson(deadline),
       "id" -> Json.toJson(id)
     )
     Json.toJson(taskMap)
