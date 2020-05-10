470aa092530181e66ca86030317fac5852da81eb
ayushibadiyani <ayushira@buffalo.edu>
2020-05-10 17:08:25 -0400

    Added Deadlines and sorting by deadlines earliest to latest

--- src/main/scala/model/database/Database.scala
+++ src/main/scala/model/database/Database.scala
@@ -9,11 +9,8 @@ class Database extends DatabaseAPI{
 
   val url = "jdbc:mysql://localhost/mysql?serverTimezone=UTC"
 
-  val username: String = "root"
-
-//    sys.env("DB_USERNAME")
-  val password: String = "ayushira"
-//    sys.env("DB_PASSWORD")
+  val username: String = sys.env("DB_USERNAME")
+  val password: String = sys.env("DB_PASSWORD")
 
   var connection: Connection = DriverManager.getConnection(url, username, password)
   setupTable()
