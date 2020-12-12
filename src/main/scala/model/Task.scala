aac1237957389aae5d3152f0cd4f503bf675c51d
ceramire@buffalo.edu <1OY$Fd3LsD#F>
2020-12-12 15:11:05 -0500

    Added the option to give a task a deadline.

--- src/main/scala/model/Task.scala
+++ src/main/scala/model/Task.scala
@@ -3,6 +3,7 @@ package model
 import play.api.libs.json.{JsValue, Json}
 
 
+
 object Task {
 
   var nextId: Int = 0
@@ -18,21 +19,22 @@ object Task {
     output
   }
 
-  def apply(title: String, description: String): Task = {
+  def apply(title: String, description: String, deadline: String): Task = {
     val thisId = nextId
     nextId += 1
-    new Task(cleanString(title), cleanString(description, 1000), thisId.toString)
+    new Task(cleanString(title), cleanString(description, 1000), cleanString(deadline, 1000), thisId.toString)
   }
 
 
 }
 
-class Task(val title: String, val description: String, val id: String) {
+class Task(val title: String, val description: String, val deadline: String, val id: String) {
 
   def asJsValue(): JsValue ={
     val taskMap: Map[String, JsValue] = Map(
       "title" -> Json.toJson(title),
       "description" -> Json.toJson(description),
+      "deadline" -> Json.toJson(deadline),
       "id" -> Json.toJson(id)
     )
     Json.toJson(taskMap)
