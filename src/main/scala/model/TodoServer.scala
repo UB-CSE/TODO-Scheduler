7dade91a2b9eb276ce2ed9f592dd16e3ee3f977b
Kaiya Hogg <kaiya.hogg@gmail.com>
2020-12-12 16:40:48 -0500

    Created text box or comments and set them to display with tasks in the todo list.

--- src/main/scala/model/TodoServer.scala
+++ src/main/scala/model/TodoServer.scala
@@ -69,8 +69,9 @@ class AddTaskListener(server: TodoServer) extends DataListener[String] {
     val task: JsValue = Json.parse(taskJSON)
     val title: String = (task \ "title").as[String]
     val description: String = (task \ "description").as[String]
+    val comment: String = (task \ "comment").as[String]
 
-    server.database.addTask(Task(title, description))
+    server.database.addTask(Task(title, description, comment))
     server.server.getBroadcastOperations.sendEvent("all_tasks", server.tasksJSON())
   }
 
