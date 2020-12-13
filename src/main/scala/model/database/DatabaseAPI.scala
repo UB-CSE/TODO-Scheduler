f408847daaff201b38dfbd976ff9c76707f41a39
fhggbhjghy <fhggbhjghy11@gmail.com>
2020-12-13 17:51:27 -0500

    Added Event to import and export JSON database following the same format in server.tasksJSON
    Modified server.taskJSON and added DatabaseAPI.getLastTaskId to map socket -> Database
    
    Added server.makeTask to remove duplicate code
    
    Added server.importTask to read JSON Strings and add external tasks to server.database

--- src/main/scala/model/database/DatabaseAPI.scala
+++ src/main/scala/model/database/DatabaseAPI.scala
@@ -6,6 +6,7 @@ trait DatabaseAPI {
 
   def addTask(task: Task): Unit
   def completeTask(taskId: String): Unit
+  def getLastTaskId(): Int
   def getTasks: List[Task]
 
 }
--- src/main/scala/model/database/TestingDatabase.scala
+++ src/main/scala/model/database/TestingDatabase.scala
@@ -11,6 +11,16 @@ class TestingDatabase extends DatabaseAPI {
     data ::= task
   }
 
+  override def getLastTaskId(): Int = {
+    var id = 0
+    for(task <- data){
+      if(task.id.toInt > id){
+        id = task.id.toInt
+      }
+    }
+    id
+  }
+
 
   override def completeTask(taskId: String): Unit = {
     data = data.filter(_.id != taskId)
